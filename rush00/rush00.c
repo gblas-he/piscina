e3424c02fb4eb0d3c573cca545ecde83768bfa03
Giovanni Blas Heredia <gblas-he@c1r9s3.42madrid.com>
2026-02-03 18:30:12 +0100

    rush00

---
+++ rush00/rush00.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jezambra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 14:40:26 by jezambra          #+#    #+#             */
+/*   Updated: 2025/11/09 14:41:05 by jezambra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	vision(int x, int y, int i, int j)
+{
+	if (i == 0 && (j == 0 || j == y - 1))
+	{
+		ft_putchar('o');
+	}
+	else if (i == x - 1 && (j == 0 || j == y - 1))
+	{
+		ft_putchar('o');
+	}
+	else if ((i == 0) || (i == x - 1))
+	{
+		ft_putchar('|');
+	}
+	else if ((j == 0) || (j == y - 1))
+	{
+		ft_putchar('-');
+	}
+	else
+	{
+		ft_putchar(' ');
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (j < y)
+	{
+		while (i < x)
+		{
+			vision(x, y, i, j);
+			i++;
+		}
+		ft_putchar(10);
+		j++;
+		i = 0;
+	}
+}
---
+++ rush00/rush02.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jezambra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 14:40:26 by jezambra          #+#    #+#             */
+/*   Updated: 2025/11/09 21:41:36 by jezambra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	vision(int x, int y, int i, int j)
+{
+	if (j == 0 && (i == 0 || i == x - 1))
+	{
+		ft_putchar('A');
+	}
+	else if (j == x - 1 && (i == 0 || i == x - 1))
+	{
+		ft_putchar('C');
+	}
+	else if ((i == 0) || (i == x - 1))
+	{
+		ft_putchar('B');
+	}
+	else if ((j == 0) || (j == y - 1))
+	{
+		ft_putchar('B');
+	}
+	else
+	{
+		ft_putchar(' ');
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (j < y)
+	{
+		while (i < x)
+		{
+			vision(x, y, i, j);
+			i++;
+		}
+		ft_putchar(10);
+		j++;
+		i = 0;
+	}
+}
